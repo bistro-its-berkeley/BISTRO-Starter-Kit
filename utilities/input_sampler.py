666bf601766c9e8feca9ea1bd1efb594d52ae5b6
sfwatergit <sid.feygin@gmail.com>
2018-12-14 22:28:59 -0800

    Don't sample cars for vehicle input

--- utilities/input_sampler.py
+++ utilities/input_sampler.py
@@ -127,7 +127,7 @@ def sample_vehicle_fleet_mix_input(num_records, gtfs_manager):
     route_agency_sample = gtfs_manager.routes.sample(num_records)
     routes = pd.Series(route_agency_sample.index.values)
     agency = pd.Series(route_agency_sample.agency_id.values)
-    vehicles = pd.Series(gtfs_manager.vehicle_types.sample(num_records, replace=True).index)
+    vehicles = pd.Series((gtfs_manager.vehicle_types.filter(like="BUS", axis=0)).sample(num_records, replace=True).index)
     df = pd.concat([agency, routes, vehicles], axis=1, ignore_index=True)
     df.columns = ["agencyId", "routeId", "vehicleTypeId"]
     return df
