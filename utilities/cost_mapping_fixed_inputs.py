5a7b2ace0b76001b97e3ce893b97cc604b74c173
Golfier <golfiervalentine@gmail.com>
2019-01-17 22:18:54 -0800

    adding first draft of exploratory analysis scripts

---
+++ utilities/cost_mapping_fixed_inputs.py
@@ -0,0 +1,136 @@
+import pandas as pd
+import numpy as np
+import itertools
+
+# Defining input features
+# 0. Route IDs
+route_id = np.arange(1340, 1352).tolist()
+
+# 1. AVAILABLE BUSES
+agencies = [217] * len(route_id)
+
+BUS_SMALL_HD = "BUS-SMALL-HD"
+BUS_STD_ART = "BUS-STD-ART"
+BUS_DEFAULT = "BUS-DEFAULT"
+
+
+# 2. MODES TO SUBSIDIZE
+ON_DEMAND_RIDE = "OnDemand_ride"
+DRIVE_TO_TRANSIT = "drive_transit"
+WALK_TO_TRANSIT = "walk_transit"
+
+# 3. AGE GROUPS
+YOUNG = "(0,25]"
+ADULTS = "(25,65]"
+SENIORS = "(65,120]"
+ALL_AGES = "(0,120]"
+
+# 4. INCOME GROUPS
+LOW_INCOME = "[0,25000]"
+ALL_INCOMES = "[0,500000]"
+
+# 4. INCENTIVE AMOUNT
+transit_subsidy = 1.00
+on_demand_ride_subsidy = 3.00
+
+# MASS TRANSIT FARES
+full_fare = 1.50
+free = 0.00
+
+
+# 6. BAU DATA FRAMES (EMPTY DF TO BE FILLED UP WHEN TESTING OTHER INPUTS)
+bau_vehicle_type_input = pd.DataFrame({"agencyId": agencies, "routeId": route_id,
+                                       "vehicleTypeId": [BUS_DEFAULT] * len(route_id)})
+bau_incentives_input = pd.DataFrame(columns=["mode", "age", "income", "amount"])
+
+bau_bus_fare_input = pd.DataFrame(columns=["agencyId", "routId", "age", "amount"])
+
+bau_frequency_input = pd.DataFrame(columns=["trip_id", "start_time", "emd_time", "headway_secs", "exact_times"])
+
+
+#######################################
+
+incentive_1 = [[DRIVE_TO_TRANSIT, None, LOW_INCOME, transit_subsidy],
+               [WALK_TO_TRANSIT, None, LOW_INCOME, transit_subsidy]]
+
+incentive_2 = [[DRIVE_TO_TRANSIT, ALL_AGES, ALL_INCOMES, transit_subsidy],
+               [WALK_TO_TRANSIT, ALL_AGES, ALL_INCOMES, transit_subsidy]]
+
+incentive_3 = [[ON_DEMAND_RIDE, ALL_AGES, ALL_INCOMES, on_demand_ride_subsidy]]
+
+incentive_4 = [[ON_DEMAND_RIDE, None, LOW_INCOME, on_demand_ride_subsidy]]
+
+incentive = [incentive_1, incentive_2, incentive_3, incentive_4]
+
+# Building the ModeIncentive input dataframes
+input_incentives_list = []
+
+for incentive_i in incentive:
+    incentives_input = pd.DataFrame(incentive_i)
+    incentives_input.columns = bau_incentives_input.columns
+
+    input_incentives_list.append(incentives_input)
+
+# Mass transit fare input
+pt_fare_1 = [[217, 1340, YOUNG, free],
+             [217, 1341, YOUNG, free],
+             [217, 1342, YOUNG, free],
+             [217, 1343, YOUNG, free],
+             [217, 1344, YOUNG, free],
+             [217, 1345, YOUNG, free],
+             [217, 1346, YOUNG, free],
+             [217, 1347, YOUNG, free],
+             [217, 1348, YOUNG, free],
+             [217, 1349, YOUNG, free],
+             [217, 1350, YOUNG, free],
+             [217, 1351, YOUNG, free],
+           [217, 1340, SENIORS, free],
+           [217, 1341, SENIORS, free],
+           [217, 1342, SENIORS, free],
+           [217, 1343, SENIORS, free],
+           [217, 1344, SENIORS, free],
+           [217, 1345, SENIORS, free],
+           [217, 1346, SENIORS, free],
+           [217, 1347, SENIORS, free],
+           [217, 1348, SENIORS, free],
+           [217, 1349, SENIORS, free],
+           [217, 1350, SENIORS, free],
+           [217, 1351, SENIORS, free],
+           [217, 1340, ADULTS, full_fare],
+           [217, 1341, ADULTS, full_fare],
+           [217, 1342, ADULTS, full_fare],
+           [217, 1343, ADULTS, full_fare],
+           [217, 1344, ADULTS, full_fare],
+           [217, 1345, ADULTS, full_fare],
+           [217, 1346, ADULTS, full_fare],
+           [217, 1347, ADULTS, full_fare],
+           [217, 1348, ADULTS, full_fare],
+           [217, 1349, ADULTS, full_fare],
+           [217, 1350, ADULTS, full_fare],
+           [217, 1351, ADULTS, full_fare]]
+
+pt_fare_2 = [[217, 1340, ALL_AGES, full_fare],
+           [217, 1341, ALL_AGES, full_fare],
+           [217, 1342, ALL_AGES, full_fare],
+           [217, 1343, ALL_AGES, full_fare],
+           [217, 1344, ALL_AGES, full_fare],
+           [217, 1345, ALL_AGES, full_fare],
+           [217, 1346, ALL_AGES, full_fare],
+           [217, 1347, ALL_AGES, full_fare],
+           [217, 1348, ALL_AGES, full_fare],
+           [217, 1349, ALL_AGES, full_fare],
+           [217, 1350, ALL_AGES, full_fare],
+           [217, 1351, ALL_AGES, full_fare]]
+
+mass_transit_fare = [pt_fare_1, pt_fare_2]
+
+# Building the PtFares input DataFrames
+
+input_bus_fare_list = []
+
+for mass_transit_fare_i in mass_transit_fare:
+    bus_fare_input = pd.DataFrame(mass_transit_fare_i)
+    bus_fare_input.columns = bau_bus_fare_input.columns
+    input_bus_fare_list.append(bus_fare_input)
+
+input_combinations = list(itertools.product(a, b))
---
+++ utilities/exploratory_analysis.py
@@ -0,0 +1,192 @@
+import logging
+import os
+import tempfile
+from pathlib import Path
+
+import docker
+import numpy as np
+import pandas as pd
+import sys
+
+import input_sampler as sampler
+from cost_mapping_fixed_inputs import input_combinations
+
+AGENCY = "sioux_faux_bus_lines"
+SCENARIO_NAME = "sioux_faux"
+DOCKER_IMAGE = "beammodel/beam-competition:0.0.1-SNAPSHOT"
+
+CMD_TEMPLATE = "--scenario {0} --sample-size {1} --iters {2}"
+DIR_DELIM = "-"
+
+FREQ_FILE = "FrequencyAdjustment.csv"
+SUB_FILE = "ModeIncentives.csv"
+FLEET_FILE = "VehicleFleetMix.csv"
+PT_FARE_FILE = "PtFares.csv"
+SCORES_PATH = ("competition", "submissionScores.csv")
+
+logger = logging.getLogger(__name__)
+
+
+def abspath2(path):
+    path = os.path.abspath(os.path.expanduser(path))  # expanduser() expands "~" into "/Users/username"
+    return path
+
+
+def only_subdir(path):
+    subdir = os.listdir(path)[0]  # Validates only returned element
+    path = os.path.join(path, subdir)
+    return path
+
+
+def docker_exists(container_id, client):
+    try:
+        client.containers.get(container_id)
+    except docker.errors.NotFound:
+        return False
+    return True
+
+
+def sample_settings(max_num_records, data_root):
+    # TODO pull out data GTFS stuff to make this pure function
+    agency_dict = sampler.scenario_agencies(Path(data_root), SCENARIO_NAME)
+    sf_gtfs_manager = sampler.AgencyGtfsDataManager(agency_dict[AGENCY])
+
+    for incentives in fixed_inputs.incentive_inputs():
+        for fares in fixed_inputs.pt_fare_inputs():
+            samplers = [sampler.sample_frequency_adjustment_input,
+                        incentives,
+                        sampler.sample_vehicle_fleet_mix_input,
+                        fares]
+
+            samples = []
+            for input_sampler in samplers:
+                num_records = np.random.randint(0, max_num_records)
+                samples.append(input_sampler(num_records, sf_gtfs_manager))
+
+    return tuple(samples)
+
+
+def save_inputs(input_dir, freq_df=None, mode_subsidy_df=None, vehicle_fleet_mix_df=None, pt_fare_df=None):
+    if freq_df is not None:
+        freq_df.to_csv(os.path.join(input_dir, FREQ_FILE), header=True, index=False)
+    if mode_subsidy_df is not None:
+        mode_subsidy_df.to_csv(os.path.join(input_dir, SUB_FILE), header=True, index=False)
+    if vehicle_fleet_mix_df is not None:
+        vehicle_fleet_mix_df.to_csv(os.path.join(input_dir, FLEET_FILE), header=True, index=False)
+    if pt_fare_df is None:
+        pt_fare_df = pd.read_csv('../submission-inputs/{0}'.format(PT_FARE_FILE))
+    pt_fare_df.to_csv(os.path.join(input_dir, PT_FARE_FILE), header=True, index=False)
+
+
+def read_scores(output_dir):
+    """Read scores from output directory as .csv file.
+    """
+    output_dir = only_subdir(only_subdir(output_dir))
+    df = pd.read_csv(os.path.join(output_dir, *SCORES_PATH), index_col="Component Name")
+    scores = df["Weighted Score"]
+    return scores
+
+
+def search_iteration(docker_cmd, data_root, input_root, output_root):
+    max_records = 10
+
+    client = docker.from_env()  # TODO consider if cleanest that this is in main?
+
+    assert os.path.isabs(data_root)
+    assert os.path.isabs(input_root)
+    assert os.path.isabs(output_root)
+
+    # Make temp dirs, race condition safe too
+    input_dir = tempfile.mkdtemp(prefix="input" + DIR_DELIM, dir=input_root)
+    output_dir = tempfile.mkdtemp(prefix="output" + DIR_DELIM, dir=output_root)
+
+    # Should be unique name here since folder is unique, also checks only one instance of delim
+    _, submission_name = os.path.basename(input_dir).split(DIR_DELIM)
+    # Add 'bm_bc_' prefix due to docker's restriction on container names
+    submission_name = "bm_bc_{}".format(submission_name)
+    # Call random input sampler
+    settings = sample_settings(max_records, data_root)
+    # Save all inputs
+    save_inputs(input_dir, *settings)
+
+    # docker_dirs = {input_dir: {"bind": "/submission-inputs", "mode": "ro"},
+    #                output_dir: {"bind": "/output", "mode": "rw"},
+    #                "/tmp-data": {"bind": "/tmp-data", "mode": "rw"},
+    #                "/var/run/docker.sock": {"bind": "/var/run/docker.sock", "mode": "rw"}}
+
+    docker_dirs = {output_dir: {"bind": "/output", "mode": "rw"},
+                   input_dir: {"bind": "/submission-inputs",
+                                                             "mode": "ro"}}
+
+    assert not docker_exists(submission_name, client)
+    logger.info('%s start' % submission_name)
+    logs = client.containers.run(DOCKER_IMAGE, command=docker_cmd, auto_remove=True, detach=False,
+                                 name=submission_name, volumes=docker_dirs,
+                                 stdout=True, stderr=True)
+
+    logger.debug(logs)
+    logger.info('%s end' % submission_name)
+
+    # scores = read_scores(output_dir)
+    # score = scores["Submission Score"]
+    # score = float(score)  # verify simple float to keep simple
+
+    paths = (input_dir, output_dir)
+    # return paths, score
+    return paths
+
+def random_search(docker_cmd, n_iters, data_root, input_root, output_root):
+    best_score = None
+    best_setting = None
+    for _ in range(n_iters):
+        # paths, score = search_iteration(docker_cmd, data_root, input_root, output_root)
+        paths = search_iteration(docker_cmd, data_root, input_root, output_root)
+
+        # Appears we are maximizing
+        # if best_score is None or score > best_score:
+        #     best_score = score
+        #     best_setting = paths
+        #
+        # result_str = 'paths: %s\nscore: %f' % (str(paths), score)
+        # logger.info(result_str)
+        logger.info("Iteration Number %s / %s"% (_, n_iters))
+
+    # return best_setting, best_score
+
+
+def main():
+    os.chdir(r"/Users/vgolfi/Documents/GitHub/Uber-Prize-Starter-Kit/utilities")
+    logging.basicConfig(level=logging.INFO)
+
+    # TODO explain why this is different than SCENARIO_NAME
+
+    # We can take these from cmd args later:
+    sample_size = "15k"
+    n_sim_iters = 1
+    seed = 123
+
+    n_search_iters = 100
+    data_root = abspath2("../reference-data")
+    input_root = abspath2("../search-input")
+    output_root = abspath2("../search-output")
+
+    os.makedirs(input_root, exist_ok=True)
+    os.makedirs(output_root, exist_ok=True)
+
+    # TODO also consider setting pyseed
+    np.random.seed(seed)
+
+    # Some prints
+    docker_cmd = CMD_TEMPLATE.format(SCENARIO_NAME, sample_size, n_sim_iters)
+    # (input_dir, output_dir), best_score = random_search(docker_cmd, n_search_iters,
+    #                                                     data_root, input_root, output_root)
+    random_search(docker_cmd, n_search_iters, data_root, input_root, output_root)
+
+    # print('Best score: %f' % best_score)
+    # print('Input: %s' % input_dir)
+    # print('Output: %s' % output_dir)
+
+
+if __name__ == "__main__":
+    combination_number = sys.argv[1]
+    main()
