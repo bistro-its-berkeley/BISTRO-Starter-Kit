473e7948869afb56e8c5a7ae85e828af6251b5c9
Golfier <golfiervalentine@gmail.com>
2019-01-20 21:20:53 -0800

    Read hosts from config + change description of arguments

--- utilities/copy_remote_results.py
+++ utilities/copy_remote_results.py
@@ -1,20 +1,30 @@
 import os
 import sys
 from os import path
+import pandas as pd
 
 def rsync_results(hostname, host_ip, output_folder_name, dest_folder):
-    return os.system('rsync -avz -e "ssh -i ~/.ssh/beam_competitions_key.pem" ubuntu@{host_ip}:/home/ubuntu/Uber-Prize-Starter-Kit/search-output-{output_folder_name}/ {dest_folder}/{output_folder_name}/{hostname} --exclude="*/ITERS/" --exclude="*/output*" --exclude="*/competition/viz/"'.format(hostname=hostname,host_ip=host_ip, output_folder_name=output_folder_name, dest_folder=dest_folder))
+    return os.system('rsync -avz -e "ssh -i ~/.ssh/beam_competitions_key.pem" ubuntu@{host_ip}:/home/ubuntu/'
+                     'Uber-Prize-Starter-Kit/search-output-{output_folder_name}/ {dest_folder}/'
+                     '{output_folder_name}/{hostname} --exclude="*/ITERS/" --exclude="*/output*" --exclude="*'
+                     '/competition/viz/"'.format(hostname=hostname,host_ip=host_ip,
+                                                 output_folder_name=output_folder_name,
+                                                 dest_folder=dest_folder))
 
 
 if __name__ == "__main__":
     # Args:
-    #   - 1: Path of the output folder name to copy
-    #   - 2: Destination folder to copy on local machine
+    #   - 1: Name of the run to copy from
+    #   - 2: Path of the destination folder to copy on local machine (a folder named after the run will be created in it)
 
     output_folder_name = sys.argv[1]
     dest_folder = sys.argv[2]
 
-    hosts = ["52.13.145.44", "52.89.179.9", "54.191.161.231", "54.218.172.167", "54.218.29.151"]
+    # Read the hosts from config file
+    config = pd.read_table("~/.ssh/config")
+    config = config["Host host1"].values
+    hosts = [i.split(" ")[1] for i in config if "HostName" in i]
+
     host_names = ["host{}".format(str(i+1)) for i in range(len(hosts)-1)]
     host_names.append('debug')
 
