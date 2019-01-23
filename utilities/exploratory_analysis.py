f0e705a2674e199b8692da071578f1a518ca254b
Golfier <golfiervalentine@gmail.com>
2019-01-22 17:13:32 -0800

    minor change

--- utilities/exploratory_analysis.py
+++ utilities/exploratory_analysis.py
@@ -173,7 +173,7 @@ def main(combination_number, name_of_exploration, input_mode):
     n_search_iters = 160
     data_root = abspath2("../reference-data")
     input_root = abspath2("../search-input")
-    output_root = abspath2("../search-output-{0}{1}".format(name_of_exploration, input_mode))
+    output_root = abspath2("../search-output-{0}-{1}".format(name_of_exploration, input_mode))
 
     os.makedirs(input_root, exist_ok=True)
     os.makedirs(output_root, exist_ok=True)
