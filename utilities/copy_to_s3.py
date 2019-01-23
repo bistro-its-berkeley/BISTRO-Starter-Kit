abde9fa77804051e6a8df91c8e023bbd7ab0fa74
Golfier <golfiervalentine@gmail.com>
2019-01-23 00:56:58 -0800

    added options for parallelization

--- utilities/copy_to_s3.py
+++ utilities/copy_to_s3.py
@@ -2,18 +2,23 @@ import boto3
 import sys
 import os
 from os import path
+from multiprocessing.pool import ThreadPool
+from datetime import datetime
 
+bucketName = "uber-prize-testing-output"
+s3 = boto3.client('s3')
+
+
+def upload(filename):
+    s3.upload_file(filename, bucketName, filename)
 
 def main(name_of_exploration):
     ## Setting working directory to Uber Prize starter kit root
     os.chdir("..")
 
-    s3 = boto3.client('s3')
-
     # search-output-all_randomrandom
     ## Finding all relevant files
     output_dir = f"search-output-{name_of_exploration}"
-    bucketName = "uber-prize-testing-output"
 
     files_to_copy = []
     for root, dirs, files in os.walk(output_dir, topdown=False):
@@ -29,12 +34,18 @@ def main(name_of_exploration):
         s3.upload_file(f_i, bucketName, f_i)
 
     # First attempt at parallelization
+    # Raises an error because joblib does not want to parallelize a function with arguments
+    # that cannot be pickled
     # Parallel(n_jobs=-2)(delayed(s3.upload_file)(f_i, bucketName, f_i) for f_i in files_to_copy)
+
+    # with multi-processing
+    # Seems to upload something but not sure if faste
+    # pool = ThreadPool(processes=2)
+    # pool.map(upload, files_to_copy)
 
 
 if __name__ == "__main__":
     name_of_exploration = sys.argv[1]
     main(name_of_exploration)
-
 
 
