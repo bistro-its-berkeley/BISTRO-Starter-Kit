74c5f03281179998ba255fdf34c4c5247c379f05
Golfier <golfiervalentine@gmail.com>
2019-01-24 23:00:44 -0800

    last minute modifications

--- utilities/copy_to_s3.py
+++ utilities/copy_to_s3.py
@@ -19,10 +19,10 @@ if __name__ == "__main__":
     output_folder_name = sys.argv[1]
 
     # Check if the output folder contains a submissionScores.csv file, i.e. of the simulation worked
-    submissions = glob(r"Uber-Prize-Starter-Kit/search-output-{output_folder_name}/*/sioux_faux/sioux_faux-15k__*/"
-                       r"competition/submissionScores.csv".format(output_folder_name=output_folder_name))
+    os.chdir("..")
+    submissions = glob(r"search-output-{output_folder_name}/*/sioux_faux/sioux_faux-15k__*/competition/submissionScores.csv".format(output_folder_name=output_folder_name))
     right_output_folders = [find_output_folder_name(submissions[i], 4) for i in range(len(submissions))]
-    all_output_folders = glob(r"Uber-Prize-Starter-Kit/search-output-{output_folder_name}/output_C*_RS*".format(
+    all_output_folders = glob(r"search-output-{output_folder_name}/output_C*_RS*".format(
         output_folder_name=output_folder_name))
 
     for folder in all_output_folders:
@@ -30,5 +30,5 @@ if __name__ == "__main__":
             rmtree(folder)
 
     # Launching s3 sync
-    os.chdir(f"../search-output-{output_folder_name}")
+    os.chdir(f"search-output-{output_folder_name}")
     os.system(f"aws s3 sync . s3://uber-prize-testing-output/search-output-{output_folder_name}")
