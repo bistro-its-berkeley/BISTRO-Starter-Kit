8c0f91351ab62ad5a7288b67c9bc2dfded31186a
Golfier <golfiervalentine@gmail.com>
2019-01-31 10:01:37 -0800

    uncommented docker stop cmd

--- utilities/run_remote.py
+++ utilities/run_remote.py
@@ -71,12 +71,12 @@ if __name__ == '__main__':
     # TODO[vgv]: Check if venv exists and if not, create!
     # Kill all containers before running simulation
     # run('sudo pkill java', connection)
-    
-    # try:
-    #     run('sudo docker stop $(docker ps -aq) -t 0', connection)
-    # except Exception as e:
-    #     print("No Docker containers to delete.")
-    #     pass
+
+    try:
+        run('sudo docker stop $(docker ps -aq) -t 0', connection)
+    except Exception as e:
+        print("No Docker containers to delete.")
+        pass
 
     # Commands to delete all search folders
     # cd / home / ubuntu / Uber - Prize - Starter - Kit & & \
