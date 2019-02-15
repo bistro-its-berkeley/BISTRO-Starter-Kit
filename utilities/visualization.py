5919a4b5c4dce79a256b0958b17a28f165bd1774
Golfier <golfiervalentine@gmail.com>
2019-02-14 21:13:00 -0800

    Comment accessibility

--- utilities/visualization.py
+++ utilities/visualization.py
@@ -1,13 +1,14 @@
 import matplotlib.pyplot as plt
 from matplotlib.cm import ScalarMappable
 
-import pandana as pdna
+# import pandana as pdna
+# import utm
+# import geopandas as gpd
+# from shapely.geometry import Point, Polygon
+# from collections import defaultdict
+# import re
+
 from lxml import etree as ET
-import utm
-import geopandas as gpd
-from shapely.geometry import Point, Polygon
-from collections import defaultdict
-import re
 import zipfile
 import gzip
 import shutil
@@ -883,157 +884,223 @@ def plot_cost_benefits(path_df, legs_df, operational_costs, trip_to_route):
     return ax
 
 
+def prepare_raw_scores(raw_scores_data):
+    # raw_scores_data = path of the submissionsScores.csv file
+    scores = pd.read_csv(raw_scores_data)
+    scores = scores.loc[:,["Component Name","Raw Score"]]
 
-####### PROCESS AND PLOT SPATIAL DATA ######
+    # Drop the `subission score` row
+    scores = scores.drop(index = 10, axis = 0)
+    scores["Component Name"] = scores["Component Name"].astype('category').cat.reorder_categories([
+           'Accessibility: Number of secondary locations accessible within 15 minutes',
+           'Accessibility: Number of work locations accessible within 15 minutes',
+           'Congestion: average vehicle delay per passenger trip',
+           'Congestion: total vehicle miles traveled',
+           'Level of service: average bus crowding experienced',
+           'Level of service: average on-demand ride wait times',
+           'Level of service: average trip expenditure - secondary',
+           'Level of service: average trip expenditure - work',
+           'Mass transit level of service intervention: costs and benefits',
+           'Sustainability: Total PM 2.5 Emissions'])
+
+    scores = scores.sort_values(by="Component Name")
+    scores.iloc[:2, 1] = scores.iloc[:2, 1].apply(np.reciprocal)
+    scores["Subscores"] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+    return scores
+
+
+def plot_raw_scores(raw_scores_data):
+    raw_scores = prepare_raw_scores(raw_scores_data)
+    sns.barplot(x="Raw Score", y="Subscores", data=raw_scores, palette=['steelblue', 'steelblue', 'lightsteelblue',
+                                                                        'lightsteelblue', 'lightsteelblue',
+                                                                        'lightsteelblue', 'skyblue', 'skyblue',
+                                                                        'lightblue', 'paleturquoise'])
+    plt.yticks(fontsize=11)
+    plt.xlabel("Raw Score")
+    plt.ylabel("Score component name")
+    plt.title("Raw Subscores", fontweight="bold", pad=12, fontsize=15)
+
+def plot_standardized_scores(scores_data_path, ):
+    sns.set_context('notebook')
+    sns.set_palette('Set1')
+    fig, ax = plt.subplots()
+    fig.set_size_inches(7, 5)
+
+    sc_fit = StandardScaler().fit(wide_scores.values)
+    sample1 = pd.Series(dict(zip(wide_scores.columns.tolist(), np.squeeze(
+        sc_fit.transform(wide_scores.loc[sample1_key].values.reshape(1, -1))).tolist())))
+    sample2 = pd.Series(dict(zip(wide_scores.columns.tolist(), np.squeeze(
+        sc_fit.transform(wide_scores.loc[sample2_key].values.reshape(1, -1))).tolist())))
+
+    # flip accessibility:
+    # sample1.iloc[0:2]=np.reciprocal(sample1.iloc[0:2])
+
+    std_raw_scores = pd.DataFrame({"Sample 1": sample1, "Sample 2": sample2})
+    # sns.barplot(data=std_raw_scores)
+    std_raw_scores.index.name = "Score Component"
+    std_raw_scores.columns.name = 'Sample'
+    std_raw_scores.index = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
+    std_raw_scores.plot(kind='barh', ax=ax)
+    # plt.axvline(x=1.0,linewidth=1, color='k', ls='dashed', label = "baseline")
+    plt.xlabel("Standardized Score")
+    ax.set_title('Policy Focus: Agnostic')
+    # plt.xlim(right = 0.7,left=-0.7)
+
+    sns.despine()
+    plt.savefig('img/random_inputs/Policy Agnostic_standardized_scores.png', format='png', dpi=150, bbox_inches="tight")
+    # plt.xlim(xmax = 1.4)
+
+
+####### PROCESS AND PLOT SPATIAL DATA ######              --> Need to be run w/ Python 2!
 
 # Defining matsim_network_to_graph(``)
-def convert_crs(c):
-    return utm.to_latlon(c[0],c[1],14,'N')
-
-
-def make_traveltime_dfs(linkstats_filename, morning_peak, evening_peak):
-    link_df = pd.read_csv(linkstats_filename,compression='gzip')
-    link_df = link_df[link_df.stat == 'AVG']
-    link_df.drop(link_df.hour[link_df.hour ==  '0.0 - 30.0'].index,inplace=True)
-    link_df.hour=link_df.hour.astype(float).astype(int)
-    morning_link_df = link_df[link_df.hour.map(lambda x: x in morning_peak)].groupby('link').mean()[['from','to','traveltime']]
-    evening_link_df = link_df[link_df.hour.map(lambda x: x in evening_peak)].groupby('link').mean()[['from','to','traveltime']]
-    return morning_link_df,evening_link_df
-
-
-def make_node_df(network_filename):
-    matsimnet = open_xml(network_filename).getroot()
-    nodes = matsimnet[1]
-    links = matsimnet[3]
-    node_data = []
-
-    # populate node dataframes
-    for node in nodes:
-        coords = convert_crs((float(node.get('x')), float(node.get('y'))))
-        node_data.append([int(node.get('id')), coords[1],coords[0]])
-    node_data = np.array(node_data)
-
-    node_df = pd.DataFrame({'x':node_data[:,1],'y':node_data[:,2]},index=node_data[:,0].astype(int))
-    node_df.index.name = 'id'
-    return node_df
-
-
-def create_pandana_net(nodes,edges):
-    return pdna.Network(nodes.x, nodes.y, edges['from'], edges['to'], edges[['traveltime']])
-
-
-class TravelTimeAccessibilityAnalysis(object):
-
-    def __init__(self,
-                 network_file,
-                 linkstats_file,
-                 population_file,
-                 utm_zone):
-        self.network_file = network_file
-        self.linkstats_file = linkstats_file
-        self.population_file = population_file
-        self.utm_zone_number = re.match("\d*", utm_zone)
-        self.utm_zone_letter = re.match("[N|S]", utm_zone)
-        self.poi_dict = self._make_poi_dict()
-
-    def _convert_crs(self, c):
-        return utm.to_latlon(c[0], c[1],
-                             self.utm_zone_number,
-                             self.utm_zone_letter)
-
-    def make_net_for_timerange(self, timerange):
-        traveltime_df = self.make_traveltime_df(timerange)
-        node_df = make_node_df(self.network_file)
-        return create_pandana_net(node_df, traveltime_df)
-
-    def make_node_df(self):
-        matsimnet = open_xml(self.network_file).getroot()
-        nodes = matsimnet[1]
-        links = matsimnet[3]
-        node_data = []
-
-        # populate node dataframes
-        for node in nodes:
-            coords = convert_crs((float(node.get('x')), float(node.get('y'))))
-            node_data.append([int(node.get('id')), coords[1], coords[0]])
-        node_data = np.array(node_data)
-
-        node_df = pd.DataFrame({'x': node_data[:, 1], 'y': node_data[:, 2]}, index=node_data[:, 0].astype(int))
-        node_df.index.name = 'id'
-        return node_df
-
-    def make_pandana_nets(self, poi_types, timeranges, max_time):
-        nets = {}
-        for label, timerange in timeranges.items():
-            net = self.make_net_for_timerange(timerange)
-            for poi_type in poi_types:
-                net.precompute(max_time)
-                poi_locs = np.array(self.poi_dict[poi_type])
-                x, y = poi_locs[:, 1], poi_locs[:, 0]
-                net.set_pois(poi_type, max_time, 10, x, y)
-                nets[label] = net
-        return nets
-
-    def make_traveltime_df(self, timerange):
-        link_df = pd.read_csv(self.linkstats_file, compression='gzip')
-        link_df = link_df[link_df.stat == 'AVG']
-        link_df.drop(link_df.hour[link_df.hour == '0.0 - 30.0'].index, inplace=True)
-        link_df.hour = link_df.hour.astype(float).astype(int)
-        traveltime_link_df = link_df[link_df.hour.map(lambda x: x in timerange)].groupby('link').mean()[
-            ['from', 'to', 'traveltime']]
-        return traveltime_link_df
-
-    def _make_poi_dict(self):
-        population_xml = open_xml(self.population_file).getroot()
-        persons = population_xml.findall('person')
-        poi_dict = defaultdict(list)
-        for person in persons:
-            for activity in person[1]:
-                actType = activity.get('type').lower()
-                coords = convert_crs([float(activity.get('x')), float(activity.get('y'))])
-                poi_dict[actType].append(coords)
-        return poi_dict
-
-
-def plot_accessibility(sample_name, network_file, bau_linkstats_file, population_file, utm_zone, poi_types, time_ranges, max_time, morning_peak, evening_peak):
-
-    ttta_bau = bau_ttaa = TravelTimeAccessibilityAnalysis(network_file,bau_linkstats_file,population_file, utm_zone)
-    nets = bau_ttaa.make_pandana_nets(poi_types,time_ranges,max_time)
-    node_df=bau_ttaa.make_node_df()
-    bau_poi_dict = bau_ttaa._make_poi_dict()
-    works = bau_poi_dict['work']
-    secondaries = bau_poi_dict['secondary']
-    evening_peak,morning_peak = make_traveltime_dfs(bau_linkstats_file, morning_peak, evening_peak)
-
-    bau_gdfs = {}
-    for label_poi,poi_data in bau_ttaa.poi_dict.items():
-        fig,ax=plt.subplots()
-        fig.set_size_inches(10,10)
-        ax.set_facecolor('k')
-        ax.set_title("{}: {} locations accessible within {} minutes".format(sample_name,label_poi.title(),max_time/60))
-        legend=fig.legend()
-        legend.set_visible(True)
-        poi_data = np.array(poi_data)
-        x,y = poi_data[:,1],poi_data[:,0]
-
-        for label_timerange,net in nets.items():
-            node_ids = net.get_node_ids(x,y)
-            net.set(node_ids)
-            a = net.aggregate(max_time, type="sum",decay="linear")
-
-        bau_gdf = gpd.GeoDataFrame(node_df, geometry=[Point(row.x, row.y) for _, row in node_df.iterrows()])
-        ax.axis('equal')
-        net.set(node_ids)
-        w = net.aggregate(max_time, type="sum", decay="linear")
-        bau_gdf["15min_{}".format(label_poi)]=w
-        bau_gdf = bau_gdf[bau_gdf["15min_{}".format(label_poi)]>0]
-        bau_gdf.plot(markersize=5, column="15min_{}".format(label_poi),ax=ax,legend=True,vmin=0,vmax=2000)
-
-        bau_gdfs[label_poi]=bau_gdf
-        ax.xaxis.set_ticks_position('none')
-        ax.yaxis.set_ticks_position('none')
-        ax.xaxis.set_ticklabels([])
-        ax.yaxis.set_ticklabels([])
-    return ax
-
-
+# def convert_crs(c):
+#     return utm.to_latlon(c[0],c[1],14,'N')
+#
+#
+# def make_traveltime_dfs(linkstats_filename, morning_peak, evening_peak):
+#     link_df = pd.read_csv(linkstats_filename,compression='gzip')
+#     link_df = link_df[link_df.stat == 'AVG']
+#     link_df.drop(link_df.hour[link_df.hour ==  '0.0 - 30.0'].index,inplace=True)
+#     link_df.hour=link_df.hour.astype(float).astype(int)
+#     morning_link_df = link_df[link_df.hour.map(lambda x: x in morning_peak)].groupby('link').mean()[['from','to','traveltime']]
+#     evening_link_df = link_df[link_df.hour.map(lambda x: x in evening_peak)].groupby('link').mean()[['from','to','traveltime']]
+#     return morning_link_df,evening_link_df
+#
+#
+# def make_node_df(network_filename):
+#     matsimnet = open_xml(network_filename).getroot()
+#     nodes = matsimnet[1]
+#     links = matsimnet[3]
+#     node_data = []
+#
+#     # populate node dataframes
+#     for node in nodes:
+#         coords = convert_crs((float(node.get('x')), float(node.get('y'))))
+#         node_data.append([int(node.get('id')), coords[1],coords[0]])
+#     node_data = np.array(node_data)
+#
+#     node_df = pd.DataFrame({'x':node_data[:,1],'y':node_data[:,2]},index=node_data[:,0].astype(int))
+#     node_df.index.name = 'id'
+#     return node_df
+#
+#
+# def create_pandana_net(nodes,edges):
+#     return pdna.Network(nodes.x, nodes.y, edges['from'], edges['to'], edges[['traveltime']])
+#
+#
+# class TravelTimeAccessibilityAnalysis(object):
+#
+#     def __init__(self,
+#                  network_file,
+#                  linkstats_file,
+#                  population_file,
+#                  utm_zone):
+#         self.network_file = network_file
+#         self.linkstats_file = linkstats_file
+#         self.population_file = population_file
+#         self.utm_zone_number = re.match("\d*", utm_zone)
+#         self.utm_zone_letter = re.match("[N|S]", utm_zone)
+#         self.poi_dict = self._make_poi_dict()
+#
+#     def _convert_crs(self, c):
+#         return utm.to_latlon(c[0], c[1],
+#                              self.utm_zone_number,
+#                              self.utm_zone_letter)
+#
+#     def make_net_for_timerange(self, timerange):
+#         traveltime_df = self.make_traveltime_df(timerange)
+#         node_df = make_node_df(self.network_file)
+#         return create_pandana_net(node_df, traveltime_df)
+#
+#     def make_node_df(self):
+#         matsimnet = open_xml(self.network_file).getroot()
+#         nodes = matsimnet[1]
+#         links = matsimnet[3]
+#         node_data = []
+#
+#         # populate node dataframes
+#         for node in nodes:
+#             coords = convert_crs((float(node.get('x')), float(node.get('y'))))
+#             node_data.append([int(node.get('id')), coords[1], coords[0]])
+#         node_data = np.array(node_data)
+#
+#         node_df = pd.DataFrame({'x': node_data[:, 1], 'y': node_data[:, 2]}, index=node_data[:, 0].astype(int))
+#         node_df.index.name = 'id'
+#         return node_df
+#
+#     def make_pandana_nets(self, poi_types, timeranges, max_time):
+#         nets = {}
+#         for label, timerange in timeranges.items():
+#             net = self.make_net_for_timerange(timerange)
+#             for poi_type in poi_types:
+#                 net.precompute(max_time)
+#                 poi_locs = np.array(self.poi_dict[poi_type])
+#                 x, y = poi_locs[:, 1], poi_locs[:, 0]
+#                 net.set_pois(poi_type, max_time, 10, x, y)
+#                 nets[label] = net
+#         return nets
+#
+#     def make_traveltime_df(self, timerange):
+#         link_df = pd.read_csv(self.linkstats_file, compression='gzip')
+#         link_df = link_df[link_df.stat == 'AVG']
+#         link_df.drop(link_df.hour[link_df.hour == '0.0 - 30.0'].index, inplace=True)
+#         link_df.hour = link_df.hour.astype(float).astype(int)
+#         traveltime_link_df = link_df[link_df.hour.map(lambda x: x in timerange)].groupby('link').mean()[
+#             ['from', 'to', 'traveltime']]
+#         return traveltime_link_df
+#
+#     def _make_poi_dict(self):
+#         population_xml = open_xml(self.population_file).getroot()
+#         persons = population_xml.findall('person')
+#         poi_dict = defaultdict(list)
+#         for person in persons:
+#             for activity in person[1]:
+#                 actType = activity.get('type').lower()
+#                 coords = convert_crs([float(activity.get('x')), float(activity.get('y'))])
+#                 poi_dict[actType].append(coords)
+#         return poi_dict
+#
+#
+# def plot_accessibility(sample_name, network_file, bau_linkstats_file, population_file, utm_zone, poi_types, time_ranges, max_time, morning_peak, evening_peak):
+#
+#     ttta_bau = bau_ttaa = TravelTimeAccessibilityAnalysis(network_file,bau_linkstats_file,population_file, utm_zone)
+#     nets = bau_ttaa.make_pandana_nets(poi_types,time_ranges,max_time)
+#     node_df=bau_ttaa.make_node_df()
+#     bau_poi_dict = bau_ttaa._make_poi_dict()
+#     works = bau_poi_dict['work']
+#     secondaries = bau_poi_dict['secondary']
+#     evening_peak,morning_peak = make_traveltime_dfs(bau_linkstats_file, morning_peak, evening_peak)
+#
+#     bau_gdfs = {}
+#     for label_poi,poi_data in bau_ttaa.poi_dict.items():
+#         fig,ax=plt.subplots()
+#         fig.set_size_inches(10,10)
+#         ax.set_facecolor('k')
+#         ax.set_title("{}: {} locations accessible within {} minutes".format(sample_name,label_poi.title(),max_time/60))
+#         legend=fig.legend()
+#         legend.set_visible(True)
+#         poi_data = np.array(poi_data)
+#         x,y = poi_data[:,1],poi_data[:,0]
+#
+#         for label_timerange,net in nets.items():
+#             node_ids = net.get_node_ids(x,y)
+#             net.set(node_ids)
+#             a = net.aggregate(max_time, type="sum",decay="linear")
+#
+#         bau_gdf = gpd.GeoDataFrame(node_df, geometry=[Point(row.x, row.y) for _, row in node_df.iterrows()])
+#         ax.axis('equal')
+#         net.set(node_ids)
+#         w = net.aggregate(max_time, type="sum", decay="linear")
+#         bau_gdf["15min_{}".format(label_poi)]=w
+#         bau_gdf = bau_gdf[bau_gdf["15min_{}".format(label_poi)]>0]
+#         bau_gdf.plot(markersize=5, column="15min_{}".format(label_poi),ax=ax,legend=True,vmin=0,vmax=2000)
+#
+#         bau_gdfs[label_poi]=bau_gdf
+#         ax.xaxis.set_ticks_position('none')
+#         ax.yaxis.set_ticks_position('none')
+#         ax.xaxis.set_ticklabels([])
+#         ax.yaxis.set_ticklabels([])
+#     return ax
+#
+#
